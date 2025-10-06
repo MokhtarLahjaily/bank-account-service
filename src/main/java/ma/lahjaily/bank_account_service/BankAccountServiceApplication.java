1893ebf0aeb60030ef42d5e6676ad2e491e188e2
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-10-06 19:42:23 +0100

    v1

--- src/main/java/ma/lahjaily/bank_account_service/BankAccountServiceApplication.java
+++ src/main/java/ma/lahjaily/bank_account_service/BankAccountServiceApplication.java
@@ -1,7 +1,17 @@
 package ma.lahjaily.bank_account_service;
 
+import ma.lahjaily.bank_account_service.entities.BankAccount;
+import ma.lahjaily.bank_account_service.enums.AccountType;
+import ma.lahjaily.bank_account_service.repositories.BankAccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static graphql.introspection.IntrospectionQueryBuilder.build;
 
 @SpringBootApplication
 public class BankAccountServiceApplication {
@@ -9,5 +19,20 @@ public class BankAccountServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankAccountServiceApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner start(BankAccountRepository bankAccountRepository) {
+		return args -> {
+			for (int i = 0; i < 10; i++) {
+				BankAccount bankAccount = BankAccount.builder()
+						.id(UUID.randomUUID().toString())
+						.type(Math.random()>0.5? AccountType.CURRENT_ACCOUNT: AccountType.SAVING_ACCOUNT)
+						.balance(10000+Math.random()*90000)
+						.createdAt(new Date())
+						.currency("MAD")
+						.build();
+				bankAccountRepository.save(bankAccount);
+			}
 
+		};
+	}
 }
---
+++ src/main/java/ma/lahjaily/bank_account_service/enums/AccountType.java
@@ -0,0 +1,5 @@
+package ma.lahjaily.bank_account_service.enums;
+
+public enum AccountType {
+    CURRENT_ACCOUNT, SAVING_ACCOUNT
+}
